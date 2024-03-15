c65b78b218fae109511be7876813597b30d0d34d
Calcifer1001 <dseidlertoken@gmail.com>
2024-03-15 11:54:06 -0300

    [ADD] create user reaction

--- src/lib/reactions.jsx
+++ src/lib/reactions.jsx
@@ -1,7 +1,7 @@
 const { getFromIndex } = VM.require("sayalot.near/widget/lib.socialDbIndex")
 const { generateMetadata, updateMetadata, buildDeleteMetadata } = VM.require(
     "sayalot.near/widget/lib.metadata"
-  );
+);
 
 let config = {}
 const ID_PREFIX = "reaction"
@@ -16,27 +16,75 @@ function getConfig() {
 }
 
 function getAction(version) {
-  const baseAction = getConfig().baseActions.reaction;
-  const versionData = version ? versions[version] : versions[currentVersion];
-  const action = baseAction + versionData.actionSuffix;
-  //   console.log(1, version, baseAction, versionData, action);
-  return getConfig().isTest ? `test_${action}` : action;
+    const baseAction = getConfig().baseActions.reaction;
+    const versionData = version ? versions[version] : versions[CURRENT_VERSION];
+    const action = baseAction + versionData.actionSuffix;
+    return getConfig().isTest ? `test_${action}` : action;
+}
+
+function normalizeOldToV_0_0_1(reaction) {
+    reaction.value.sbts = ["public"];
+
+    return reaction;
+}
+
+function normalizeId(oldFormatId) {
+    const split = oldFormatId.split("-")
+    const createdAt = split[split.length - 1]
+    split.pop()
+    split.shift() // Removes first
+    const accountId = split.join("-")
+    return `reaction/${accountId}/${createdAt}`
+}
+
+function normalizeFromV0_0_1ToV0_0_2(reaction, extraParams) {
+    const { elementReactedId } = extraParams
+    const reactionData = {
+        emoji: reaction.value.reaction,
+        elementReactedId,
+    }
+
+    const split = reaction.value.reactionId.split("-")
+    const createdTimestamp = parseInt(split[split.length - 1])
+
+    const metadata = {
+        id: normalizeId(reaction.value.reactionId),
+        author: reaction.accountId,
+        createdTimestamp: createdTimestamp,
+        lastEditTimestamp: createdTimestamp,
+        versionKey: "v0.0.2"
+    }
+    return {
+        ...reaction,
+        value: {
+            reactionData,
+            metadata
+        }
+    }
+}
+
+function normalizeFromV0_0_2ToV0_0_3(reaction) {
+    return reaction
 }
 
 const versions = {
     old: {
         normalizationFunction: normalizeOldToV_0_0_1,
-        suffixAction: "",
+        actionSuffix: "",
     },
-    "0.0.1": {
+    "v0.0.1": {
         normalizationFunction: normalizeFromV0_0_1ToV0_0_2,
-        suffixAction: `_v0.0.1`,
+        actionSuffix: `_v0.0.1`,
     },
+    "v0.0.2": {
+        normalizationFunction: normalizeFromV0_0_2ToV0_0_3,
+        actionSuffix: `_v0.0.2`,
+    }
 };
 
 function fillAction(version) {
-    const baseAction = getConfig().baseActions.emoji
-    const filledAction = baseAction + version.suffixAction
+    const baseAction = getConfig().baseActions.reaction
+    const filledAction = baseAction + version.actionSuffix
     return isTest ? `test_${filledAction}` : filledAction
 }
 
@@ -46,67 +94,61 @@ function getReactionBlackListByBlockHeight() {
 
 function filterInvalidReactions(reactions) {
     return reactions
-        .filter((reaction) => reaction.value.reactionId) // Has id
+        .filter((reaction) => reaction.value.reactionId || reaction.value.metadata.id) // Has id
         .filter(
             (reaction) =>
                 !getReactionBlackListByBlockHeight().includes(reaction.blockHeight) // Blockheight is not in blacklist
         );
 }
 
-function getReactions(action, elementReactedId) {
-    return getFromIndex(action, elementReactedId)
-}
-
-function normalizeEmoji(emoji, versionsIndex) {
+function normalizeReaction(reaction, versionsIndex, elementReactedId) {
+    const extraParams = { elementReactedId }
     const versionsKeys = Object.keys(versions)
     for (let i = versionsIndex; i < versionsKeys.length; i++) {
         const version = versions[versionsKeys[i]]
-        emoji = version.normalizationFunction(emoji)
+        reaction = version.normalizationFunction(reaction, extraParams)
     }
-    return emoji
+    return reaction
 }
 
-function getLatestEdits(emojis) {
-    return emojis.filter((obj) => {
-        const userLatestInteraction = emojis.find(
-            (emoji) => emoji.accountId === obj.accountId
+function getLatestEdits(reactions) {
+    return reactions.filter((obj) => {
+        const userLatestInteraction = reactions.find(
+            (reaction) => reaction.accountId === obj.accountId
         );
         return JSON.stringify(userLatestInteraction) === JSON.stringify(obj);
     });
 }
 
-function getEmojisNormalized(elementReactedId) {
-    return Object.keys(versions).map((version) => {
+function getReactionsNormalized(elementReactedId) {
+    return Object.keys(versions).map((version, versionIndex) => {
         const action = fillAction(versions[version]);
-        return getReactions(action, elementReactedId).then((allReactions) => {
+        return getFromIndex(action, elementReactedId).then((allReactions) => {
             const validReactions = filterInvalidReactions(allReactions);
 
             const latestEdits = getLatestEdits(validReactions);
 
-            const normalizedEmojis = latestEdits.map((emoji) => normalizeEmoji(emoji, versionIndex))
+            const normalizedReactions = latestEdits.map((reaction) => normalizeReaction(reaction, versionIndex, elementReactedId))
 
-            return normalizedEmojis
+            return normalizedReactions
 
         });
     });
 }
 
 function groupReactions(reactions, loggedUserAccountId) {
-    const userReaction = undefined;
+    const userEmoji = undefined;
     const accountsGroupedByReaction = {};
-    console.log(0, reactions)
     reactions.forEach((reaction) => {
+        const emoji = reaction.value.reactionData.emoji.split(" ")[0];
         if (reaction.accountId === loggedUserAccountId) {
-            userReaction = reaction;
+            userEmoji = emoji;
         }
-        console.log(1, reaction)
-        const emoji = reaction.value.reaction.split(" ")[0];
         if (!accountsGroupedByReaction[emoji]) {
             accountsGroupedByReaction[emoji] = [];
         }
         accountsGroupedByReaction[emoji].push(reaction.accountId);
     });
-
     const reactionsStatistics = Object.keys(accountsGroupedByReaction).map(
         (reaction) => {
             return {
@@ -116,7 +158,7 @@ function groupReactions(reactions, loggedUserAccountId) {
         }
     );
 
-    return { reactionsStatistics, userReaction };
+    return { reactionsStatistics, userEmoji };
 }
 
 function getInitialEmoji() {
@@ -140,7 +182,7 @@ function getEmojis() {
 
 function getReactions(config, elementReactedId, loggedUserAccountId) {
     setConfig(config)
-    const normReactionsPromise = getEmojisNormalized(elementReactedId);
+    const normReactionsPromise = getReactionsNormalized(elementReactedId);
 
     return Promise.all(normReactionsPromise).then((normReactions) => {
         const lastReactions = normReactions.flat()
@@ -152,10 +194,10 @@ function getReactions(config, elementReactedId, loggedUserAccountId) {
 
 function validateEmoji(emoji) {
     const errArrMessage = [];
-    if(!emoji) {
+    if (!emoji) {
         errArrMessage.push("You can only react with an emoji")
     }
-    if(!getEmojis().includes(emoji)) {
+    if (!getEmojis().includes(emoji)) {
         errArrMessage.push(`The emoji ${emoji} is not available`)
     }
     return errArrMessage
@@ -165,7 +207,7 @@ function composeData(reaction) {
     return {
         index: {
             [getAction()]: JSON.stringify({
-                key: "main",
+                key: reaction.reactionData.elementReactedId,
                 value: {
                     ...reaction,
                 },
@@ -187,13 +229,13 @@ function createReaction(config, emoji, elementReactedId, author, onCommit, onCan
     if (errors && errors.length) {
         return { error: true, data: errors };
     }
-    idPrefix, author, sbt, versionKey
+
     const metadata = generateMetadata({
         idPrefix: ID_PREFIX,
         author,
         versionKey: CURRENT_VERSION
     })
-    
+
     const reaction = {
         reactionData: {
             emoji,
@@ -209,6 +251,9 @@ function createReaction(config, emoji, elementReactedId, author, onCommit, onCan
         onCommit,
         onCancel,
     });
+
+    return { error: false, data: "Reaction created successfully" };
+
 }
 
 return { getEmojis, getReactions, createReaction }
