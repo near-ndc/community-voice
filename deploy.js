06994883b068344f28853eb0088bbbb0410b5df0
Calcifer1001 <dseidlertoken@gmail.com>
2024-03-28 13:20:18 -0300

    wip: get all files

--- deploy.js
+++ deploy.js
@@ -1,10 +1,27 @@
+const path = require("path");
+const { readdirSync }   = require("fs");
+
+function ThroughDirectory(Directory) {
+    readdirSync(Directory).forEach(File => {
+        const Absolute = Path.join(Directory, File);
+        if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+        else return Files.push(Absolute);
+    });
+}
+
+ThroughDirectory("./input/directory/");
+
 function getAllFiles() {
     const foldersToExclude = ["bosTests", "testingWidgets", "tests"]
     const filesToExclude = ["HelloCV.jsx"]
+
+    readdirSync("./src").forEach(file => {
+        console.log(file)
+    }
 }
 
 async function run() {
-    // getAllFiles()
+    getAllFiles()
     // connectToNear()
     // callSetFunction()
 }
