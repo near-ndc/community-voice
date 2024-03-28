19c1aebf4d4f1d03e78a620f58a3d301a027c0a1
Calcifer1001 <dseidlertoken@gmail.com>
2024-03-28 13:15:54 -0300

    wip: deployer

---
+++ deploy.js
@@ -0,0 +1,12 @@
+function getAllFiles() {
+    const foldersToExclude = ["bosTests", "testingWidgets", "tests"]
+    const filesToExclude = ["HelloCV.jsx"]
+}
+
+async function run() {
+    // getAllFiles()
+    // connectToNear()
+    // callSetFunction()
+}
+
+run()
